9553b8ee9b77137dfe11de6c596fcb3b87cd0f42
SidisLiveYT <SidisLiveYT@users.noreply.github.com>
2022-03-20 10:47:28 +0000

    deploy: dd9a1c1907f2aac299991acd46da8a0c3800d23c

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const vimeoHTML = require('../src copy/Classes/vimeo-html')
-
-module.exports = {
-  default: { vimeo: vimeoHTML, html: vimeoHTML.html },
-  vimeo: vimeoHTML,
-  html: vimeoHTML.html,
-}
--- src/index.mjs
+++
@@ -1,5 +0,0 @@
-import handlers from './index.js'
-
-export default handlers.default
-export const vimeo = handlers.vimeo
-export const html = handlers.html
